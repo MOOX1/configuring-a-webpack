0961f64cae8b0a8e9f0765fdd69a1cc9e6b61d6e
MOOX1 <vitormeneses87@gmail.com>
2022-02-21 17:21:54 -0300

    starting the configuration

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import button from "./Button";
+
+const App = () => {
+    return (
+        <div>
+            <p>Vitor Meneses de Jesus</p>
+            <p>Julia Viera</p>
+            <h1>teste</h1>
+            <button>Olá</button>
+        </div>
+    )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlwebPackPlugin = require("Html-webpack-plugin")
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
-    }
+    },
+module: {
+    rules: [
+        {
+            test:/\.(js|jsx)$/,
+            exclude: /node-module/,
+            use: {
+                loader: "babel-loader"
+            }
+        }
+    ]
+},
+plugins: [
+    new HtmlwebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    })
+]
 }
+
+
+
